8e0495b7c7c3c5548308746e05bc81f8a1711f92
王宇逸 <Strawberry_Str@hotmail.com>
2019-08-31 23:07:39 +0800

    Add Loading bar.

--- Berrysoft.Pages/Startup.cs
+++ Berrysoft.Pages/Startup.cs
@@ -1,6 +1,7 @@
 using Berrysoft.Pages.Data;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace Berrysoft.Pages
 {
@@ -8,11 +9,13 @@ namespace Berrysoft.Pages
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLoadingBar();
             services.AddSingleton<ILocalizationService, LocalizationService>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
+            app.UseLoadingBar();
             app.AddComponent<App>("app");
         }
     }

4e59783a1193a5ea50770d5030a5f9d07385fc7f
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-03 00:31:43 +0800

    Add DataGrid.

---
+++ Berrysoft.Pages/Shared/DataGridSortDirection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Berrysoft.Pages.Shared
+{
+    public enum DataGridSortDirection
+    {
+        None,
+        Ascending,
+        Descending
+    }
+
+    static class DataGridSortHelper
+    {
+        public static IEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector, DataGridSortDirection direction)
+        {
+            return direction switch
+            {
+                DataGridSortDirection.Ascending => source.OrderBy(keySelector),
+                DataGridSortDirection.Descending => source.OrderByDescending(keySelector),
+                _ => source
+            };
+        }
+    }
+}

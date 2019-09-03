8b7d392523eec796f4c3e1695094a80b925cecf8
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-03 20:45:31 +0800

    整理DataGrid代码

---
+++ Berrysoft.Pages/DataGrid/DataGridBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace Berrysoft.Pages.DataGrid
+{
+    public class DataGridBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable Items { get; set; }
+
+        protected IEnumerable DisplayItems { get; set; }
+
+        [Parameter]
+        public RenderFragment Headers { get; set; }
+
+        public event EventHandler<string> ColumnSorted;
+
+        protected override void OnParametersSet()
+        {
+            DisplayItems = Items;
+        }
+
+        public void SetColumnSortDirection(string column, DataGridSortDirection direction)
+        {
+            if (direction != DataGridSortDirection.None)
+            {
+                DisplayItems = Items.OfType<object>().OrderBy(item => item.GetType().GetProperty(column).GetValue(item), direction);
+            }
+            else
+            {
+                DisplayItems = Items;
+            }
+            ColumnSorted?.Invoke(this, column);
+            StateHasChanged();
+        }
+    }
+
+    public class DataGridBase<T> : DataGridBase
+    {
+        [Parameter]
+        public RenderFragment<T> ChildContent { get; set; }
+    }
+}
--- Berrysoft.Pages/DataGrid/DataGridSortDirection.cs
+++ Berrysoft.Pages/DataGrid/DataGridSortDirection.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Berrysoft.Pages.Shared
+namespace Berrysoft.Pages.DataGrid
 {
     public enum DataGridSortDirection
     {

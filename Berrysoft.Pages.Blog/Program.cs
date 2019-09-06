e4b31216199ad5d5537fd1b3b7334c13cca1b4b0
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-06 17:40:10 +0800

    自动改名并增加index的工具

---
+++ Berrysoft.Pages.Blog/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Berrysoft.Pages.Data;
+
+namespace Berrysoft.Pages.Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var title = args[0];
+                var filename = args[1];
+                var file = new FileInfo(filename);
+                var (fn, fnex) = GetFilename(file);
+                var post = new BlogPost
+                {
+                    Title = title,
+                    Date = DateTime.Now,
+                    Filename = fn,
+                    Type = GetPostType(file)
+                };
+                string json = JsonSerializer.Serialize(post, new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                });
+                if (file.Exists)
+                {
+                    file.MoveTo(Path.Combine(file.Directory.FullName, fnex));
+                }
+                Console.WriteLine(json);
+            }
+        }
+
+        static (string, string) GetFilename(FileInfo file)
+        {
+            string[] slices = file.Name.Split('.');
+            string name = string.Join('.', slices[..^1]);
+            var now = DateTime.Now;
+            var filename = $"{name}_{now.Year}_{now.Month}_{now.Day}";
+            var filenameWithEx = $"{name}_{now.Year}_{now.Month}_{now.Day}.{slices[^1]}";
+            switch (slices[^1])
+            {
+                case "txt":
+                case "htm":
+                case "md":
+                    return (filename, filenameWithEx);
+                default:
+                    return (filenameWithEx, filenameWithEx);
+            }
+        }
+
+        static BlogPostType GetPostType(FileInfo file)
+        {
+            string[] slices = file.Name.Split('.');
+            return slices[^1] switch
+            {
+                "htm" => BlogPostType.Html,
+                "md" => BlogPostType.Markdown,
+                _ => BlogPostType.Text
+            };
+        }
+    }
+}

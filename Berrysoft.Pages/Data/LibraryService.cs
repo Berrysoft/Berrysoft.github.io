a6d2d01976198c1e50b7189aabb539bcd878062d
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-08 19:34:56 +0800

    修复开源库排序bug

--- Berrysoft.Pages/Data/LibraryService.cs
+++ Berrysoft.Pages/Data/LibraryService.cs
@@ -7,6 +7,7 @@ namespace Berrysoft.Pages.Data
         public string Name { get; set; }
         public string Url { get; set; }
         public string License { get; set; }
+        public string LicenseUrl { get; set; }
     }
 
     public class LibraryService : EnumerableLoaderService<LibraryBox>
--- Berrysoft.Pages/HighlightJs/HighlightJsEngine.cs
+++ Berrysoft.Pages/HighlightJs/HighlightJsEngine.cs
@@ -4,13 +4,13 @@ namespace Berrysoft.Pages.HighlightJs
 {
     public class HighlightJsEngine : IHighlightJsEngine
     {
-        protected IJSRuntime JSRuntime { get; set; }
+        protected IJSInProcessRuntime JSRuntime { get; set; }
 
-        public HighlightJsEngine(IJSRuntime jSRuntime) => JSRuntime = jSRuntime;
+        public HighlightJsEngine(IJSRuntime jSRuntime) => JSRuntime = (IJSInProcessRuntime)jSRuntime;
 
         public string Run(string language, string code)
         {
-            return ((IJSInProcessRuntime)JSRuntime).Invoke<string>("highlight", language, code);
+            return JSRuntime.Invoke<string>("highlight", language, code);
         }
     }
 }

d69c3606f0838413f12e3e6283845874f1669629
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-13 00:48:41 +0800

    调整字符串排序

--- Berrysoft.Pages/DataGrid/DataGridSortDirection.cs
+++ Berrysoft.Pages/DataGrid/DataGridSortDirection.cs
@@ -17,10 +17,29 @@ namespace Berrysoft.Pages.DataGrid
         {
             return direction switch
             {
-                DataGridSortDirection.Ascending => source.OrderBy(keySelector),
-                DataGridSortDirection.Descending => source.OrderByDescending(keySelector),
+                DataGridSortDirection.Ascending => source.OrderBy(keySelector, UniversalComparer<TKey>.Default),
+                DataGridSortDirection.Descending => source.OrderByDescending(keySelector, UniversalComparer<TKey>.Default),
                 _ => source
             };
+        }
+    }
+
+    class UniversalComparer<T> : IComparer<T>
+    {
+        public static UniversalComparer<T> Default { get; } = new UniversalComparer<T>();
+
+        private UniversalComparer() { }
+
+        public int Compare(T x, T y)
+        {
+            if (x is string)
+            {
+                return StringComparer.OrdinalIgnoreCase.Compare(x, y);
+            }
+            else
+            {
+                return Comparer<T>.Default.Compare(x, y);
+            }
         }
     }
 }

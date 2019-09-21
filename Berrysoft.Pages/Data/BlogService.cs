1f5077024040f077943b1d6653cf38e6c4379600
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-21 14:59:16 +0800

    使用rss

--- Berrysoft.Pages/Data/BlogService.cs
+++ Berrysoft.Pages/Data/BlogService.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+using System.Xml;
 using Berrysoft.Pages.HighlightJs;
 using Berrysoft.Pages.Katex;
 using Markdig;
@@ -14,22 +16,57 @@ namespace Berrysoft.Pages.Data
     {
         public string? Title { get; set; }
         public DateTime Date { get; set; }
+        public string? Description { get; set; }
         public string? Filename { get; set; }
     }
 
-    public class BlogService : EnumerableLoaderService<BlogPost>
+    public class BlogService
     {
+        protected HttpClient Http { get; set; }
+
         protected IHighlightJsEngine HighlightJsEngine { get; set; }
 
         protected IKatexEngine KatexEngine { get; set; }
 
         protected MarkdownPipeline MarkdigPipeline { get; set; }
 
-        public BlogService(HttpClient http, IHighlightJsEngine highlightJsEngine, IKatexEngine katexEngine) : base("blogdata/index.json", http)
+        public BlogService(HttpClient http, IHighlightJsEngine highlightJsEngine, IKatexEngine katexEngine)
         {
+            Http = http;
             HighlightJsEngine = highlightJsEngine;
             KatexEngine = katexEngine;
             MarkdigPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseHighlightJs(HighlightJsEngine).UseKatex(KatexEngine).Build();
+        }
+
+        public IEnumerable<BlogPost>? Data { get; private set; }
+        private static readonly SemaphoreLocker blogsLocker = new SemaphoreLocker();
+
+        public ValueTask LoadDataAsync()
+        {
+            if (Data == null)
+            {
+                return blogsLocker.LockAsync(async () =>
+                {
+                    if (Data == null)
+                    {
+                        using var response = await Http.GetAsync("blogdata/rss.xml");
+                        using var stream = await response.Content.ReadAsStreamAsync();
+                        using var reader = XmlReader.Create(stream);
+                        var feed = SyndicationFeed.Load(reader);
+                        Data = feed.Items.Select(item => new BlogPost
+                        {
+                            Title = item.Title?.Text,
+                            Description = item.Summary?.Text,
+                            Date = item.LastUpdatedTime.LocalDateTime,
+                            Filename = item.Links?.FirstOrDefault()?.Uri?.LocalPath?.Split('/')?.LastOrDefault()
+                        });
+                    }
+                });
+            }
+            else
+            {
+                return new ValueTask();
+            }
         }
 
         public async ValueTask<(BlogPost?, string?)> GetBlogPostAsync(string filename)

3b80a779ad75d9125d09df30a3522416d839c160
Berrysoft <Strawberry_Str@hotmail.com>
2020-02-02 18:38:41 +0800

    Update packages.

--- Berrysoft.Pages/Program.cs
+++ Berrysoft.Pages/Program.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System.Threading.Tasks;
+using Berrysoft.Pages.Data;
+using Berrysoft.Pages.HighlightJs;
+using Berrysoft.Pages.Katex;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace Berrysoft.Pages
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main()
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault();
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddLoadingBar();
+            builder.Services.AddSingleton<ILocalStorage, LocalStorage>();
+            builder.Services.AddSingleton<LocalizationService>();
+            builder.Services.AddSingleton<ThemeService>();
+            builder.Services.AddSingleton<ProjectService>();
+            builder.Services.AddSingleton<GitHubEventService>();
+            builder.Services.AddSingleton<LinkService>();
+            builder.Services.AddSingleton<CounterService>();
+            builder.Services.AddSingleton<TeaService>();
+            builder.Services.AddSingleton<BlogService>();
+            builder.Services.AddHighlightJs();
+            builder.Services.AddKatex();
+            builder.Services.AddSingleton<LibraryService>();
+            await builder.Build().UseLoadingBar().UseLocalTimeZone().RunAsync();
         }
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
     }
 }

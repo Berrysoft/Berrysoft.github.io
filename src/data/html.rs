58223421aee312a3cff44aadf9f16db8a1ff3750
Berrysoft <Strawberry_Str@hotmail.com>
2021-04-25 17:23:20 +0800

    Reduce DomParser usage.

--- src/data/html.rs
+++ src/data/html.rs
@@ -1,14 +1,8 @@
 use crate::*;
-use std::iter::FromIterator;
-use web_sys::*;
 use yew::virtual_dom::VNode;
 
 pub fn parse_html(s: &str) -> Html {
-    let dom = DomParser::new()
-        .unwrap()
-        .parse_from_string(s, SupportedType::TextHtml)
-        .unwrap();
-    let body = dom.body().unwrap();
-    let nodes = body.child_nodes();
-    VNode::from_iter((0..nodes.length()).map(|i| VNode::VRef(nodes.get(i).unwrap())))
+    let div = yew::utils::document().create_element("div").unwrap();
+    div.set_inner_html(s);
+    VNode::VRef(div.into())
 }
